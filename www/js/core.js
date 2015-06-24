b1d24416b11f36c7ba1a105b23426da5e09a2892
djokinen <djokinen@yahoo.com>
2015-06-24 09:04:31 -0400

    merge from dynamic project
    merge components from other dynamic project

--- www/js/core.js
+++ www/js/core.js
@@ -4,6 +4,8 @@
 
 initSchedule = function () {
 
+	_loadData();
+
 	_showCurrentSchedule();
 
 	$("#prev_week").click(function (e) {
@@ -16,6 +18,34 @@ initSchedule = function () {
 		_showNextWeek();
 	});
 };
+
+_loadData = function () {
+	// Assign handlers immediately after making the request, and remember the jqxhr object for this request
+	var jqxhr = $.getJSON("data/data.json", function (json) {
+		_standings(json.qsbl.standings);
+		// _schedule(json.qsbl.schedule);
+	})
+	.done(function () { console.log("done"); })
+	.fail(function () { console.log("fail"); })
+	.always(function () { console.log("always"); });
+
+	// perform other work here ...
+
+	// Set another completion function for the request above
+	jqxhr.complete(function () { console.log("complete"); });
+};
+
+function _standings(standings) {
+	var items = [];
+	$.each(standings, function (key, val) {
+		items.push('<tr><td>' + key + '</td><td>' + val.w + '</td><td>' + val.l + '</td><td>' + val.t + '</td><td>' + ((val.w * 2) + (val.t)) + '</td></tr>');
+	});
+
+	$('<tbody/>', {
+		html: items.join('')
+	}).appendTo('#standings');
+}
+
 
 _showCurrentSchedule = function () {
 	var d = new Date();
